40a56cb53540a843678dbaa2f9b41a7b1ac01630
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-01-28 19:08:08 +0300

    creat exc1
    add forms and select and option

---
+++ calc/exc1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>exc1</title>
+</head>
+
+<body>
+    <div>
+        <form action="example6.php" method="POST">
+            <input type="text" name="num1"><select name="">
+                <option value=""></option>
+                <option value="">*</option>
+                <option value="">-</option>
+                <option value="">+</option>
+                <option value="">% </option>
+            </select>
+            <input type="text" name="num2">
+            <input type="submit" value="OK">
+        </form>
+    </div>
+</body>
+
+</html>
