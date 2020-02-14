549eb0cf5e4161835ea89e1e24ac8d7ccab57d34
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-02-14 21:04:49 +0300

    create date

---
+++ date/date2.php
@@ -0,0 +1,12 @@
+<?php
+echo date("M-d-y", mktime(0,0,0,12,20,2007));
+echo "<br>";
+echo date("M-d-Y", mktime(0,0,0,12,32,2007));
+echo "<br>";
+echo date("M-d-Y", mktime(0,0,0,13,1,2007));
+echo "<br>";
+echo date("M-d-Y", mktime(0,0,0,1,1,2007));
+echo "<br>";
+echo date("M-d-Y", mktime(0,0,0,1,1,07));
+
+?>
