009f6f6c3e66da41303deabb0e5a3cda0417d40c
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-02-18 21:04:14 +0300

    creat new folder
    send file in new folder

---
+++ folder/g.php
@@ -0,0 +1,8 @@
+<?php
+if (is_uploaded_file($_FILES['userfile']['tmp_name'])) {
+
+
+    copy($_FILES['userfile']['tmp_name'], "file\\" . $_FILES['userfile']['name']);
+}
+
+// "file\\ папка куда сохраняется файл"
