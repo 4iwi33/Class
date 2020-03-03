1e67a89c6afc46088257f022ede8acf4e441ef6f
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-03-03 19:55:46 +0300

    updt
    add $s in echo or
    adding :$s = $_POST['mail'];

--- regular_exprasion/smile2/smile.php
+++ regular_exprasion/smile2/smile.php
@@ -1,15 +1,15 @@
 <?php
-$arr = [
+$arr = [ //создаём массив
  "/\:\-\)/",
  "/\:\-\(/"
 ];
 
-$s = $_POST['mail'];
+// $s = $_POST['mail'];
 
-$arr2 = [
+$arr2 = [ //создаём второй массив
     '<img src="smile.png" width="30px">',
     '<img src="evil.png" width="30px">'
 ];
 
-echo preg_replace($arr, $arr2, $s);
+echo preg_replace($arr, $arr2, $_POST['mail']);
 ?>
