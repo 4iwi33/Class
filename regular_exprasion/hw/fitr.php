6be355c61bbbde4faf65beac5fc5535493bc852a
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-03-03 18:57:26 +0300

    create hw

---
+++ regular_exprasion/hw/fitr.php
@@ -0,0 +1,15 @@
+<?php
+$arr_words = file('words.txt');//file — Читает содержимое файла и помещает его в массив
+
+
+
+foreach ($arr_words as &$word) {
+    $word = trim($word);
+}
+$words = implode('|', $arr_words);
+
+if (preg_match("/$words/ui", $_POST['filter'])) {
+    echo "False";
+} else {
+    echo "Succses";
+}
