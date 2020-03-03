056568aa6d7619af47fcc8f3cc287d429ba16727
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-03-03 19:49:39 +0300

    updt

--- regular_exprasion/smile2/smile.php
+++ regular_exprasion/smile2/smile.php
@@ -8,9 +8,8 @@ $s = $_POST['mail'];
 
 $arr2 = [
     '<img src="smile.png" width="30px">',
-    '<img src="evil.png"  width="30px">'
+    '<img src="evil.png" width="30px">'
 ];
 
 echo preg_replace($arr, $arr2, $s);
-
-?> 
+?>
