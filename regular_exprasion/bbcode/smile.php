762def6209acd2211aeb8a358bd2cc0859093333
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-03-04 22:23:47 +0300

    change

--- regular_exprasion/bbcode/smile.php
+++ regular_exprasion/bbcode/smile.php
@@ -4,15 +4,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>smile</title>
 </head>
 
 <body>
     <?php
-    $s = $_POST['mail'];
-    $temp_plate = "/\[b\](.*)\[\/b\]/";
-    $newcode = '<b>$1</b>';
+    $s = $_POST['mail']; //метод: post ключ:mail
+    //                    [b\]     [\/b\] 
+    $temp_plate = "/\[i\]\[b\](.*)\[\/b\]\[\/i\]/"; //постоянно экранипуемся
+    $newcode = '<i><b>$1</b></i>';
 
+    // temp_plate - что хотим изменить
+    // $newcode - как хотм изменить (курсив и жирный)
+    // $s - где хотим изменить
     echo preg_replace($temp_plate, $newcode, $s);
     ?>
 
