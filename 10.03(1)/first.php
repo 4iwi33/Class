c3c321c53207e2ee16c4713d0cd3d291e8e8995e
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-03-10 19:20:52 +0300

    create new file

---
+++ 10.03(1)/first.php
@@ -0,0 +1,18 @@
+<?php
+$file = "seccond.xml";
+$a = $_POST['msg'];
+$b = $_POST['name'];
+$c = getdate();
+
+$str = <<<XML
+
+<msg>
+    <text>$a</text>
+    <name>$b</name>
+    <date>$c</date>
+</msg>
+XML;
+
+file_put_contents($file, $str, FILE_APPEND);
+
+?>
---
+++ 10.03(1)/form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+
+<body>
+    <form action="first.php" method="post">
+        <textarea name="msg" cols="30" rows="10"></textarea><br>
+        <input type="text" name="name"><br>
+        <input type="submit" value="ok">
+    </form>
+</body>
+
+</html>
