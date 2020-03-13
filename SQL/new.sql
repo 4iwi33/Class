13d48305d62672cb5153489f0ddfba44672bec93
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-03-13 20:41:11 +0300

    updt

--- SQL/new.sql
+++ SQL/new.sql
@@ -1,4 +1,12 @@
 SELECT *
- FROM zptable; 
+FROM zptable;
 
- -- не найти у кого зп не ровна 100
+-- не найти у кого зп не ровна 100
+
+
+
+
+ --найти тех у кого зп больше 100 рублей и работает в отделе  'Director'
+SELECT *
+FROM zptable
+WHERE ZP >=100 AND Profession ='Director';
