02f14715201e889397b9401705bbc135739fe635
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-03-13 20:55:52 +0300

    updt

--- SQL/new.sql
+++ SQL/new.sql
@@ -6,7 +6,35 @@ FROM zptable;
 
 
 
- --найти тех у кого зп больше 100 рублей и работает в отделе  'Director'
+--найти тех у кого зп больше 100 рублей и работает в отделе  'Director'
 SELECT *
 FROM zptable
 WHERE ZP >=100 AND Profession ='Director';
+
+--NOT=не  AND=и OR=или 
+
+--найти всех у кого зп не менее 100 р.
+SELECT *
+FROM zptable
+WHERE NOT ZP <100;
+
+--отсортировать таблицы  от малого к большому (Order by)
+SELECT *
+FROM zptable
+Order by ZP
+
+--ASC сортировать от меньшего к большему
+--DESC сортировать от большего к меньшему
+
+--сортировка от большего к меньшему
+SELECT *
+FROM zptable
+Order by ZP DESC
+
+--сортировка по нескольким полям
+
+--отсортировать по проффесиям в алф порядке и по зп в порядке убывания 
+SELECT *
+FROM zptable
+Order by Profession ASC,
+ ZP DESC;
