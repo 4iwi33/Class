3fe09b56c61427655c54c4cfe9574e92b74e8ee9
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-03-17 20:00:44 +0300

    updt

--- SQL/17.03/note.sql
+++ SQL/17.03/note.sql
@@ -1,8 +1,44 @@
+--R--
+
 --добавили 'bear'
-INSERT INTO first (Name)
-VALUES ('bear');
+INSERT INTO first
+    (Name)
+VALUES
+    ('bear');
 
 
 --добавили товар и цену 
-INSERT INTO first (Name, price)
-VALUES ('free', 2);
+INSERT INTO first
+    (Name, price)
+VALUES
+    ('free', 2);
+
+--Update--
+
+
+--присвоили всем товарам цену = 30
+UPDATE first Set price = 30;
+
+
+--присвоили 22 тому id который идёт 5 по счёту
+UPDATE first Set price = 22
+WHERE  id = 5;
+
+--присвоили пятому и второму 
+UPDATE first Set price = 38
+WHERE  id = 5
+    OR
+    id = 2;
+
+--Delete--
+
+--удалим 5 id
+DELETE FROM first 
+WHERE id = 5;
+
+--удалим price
+DELETE FROM first 
+WHERE price >20;
+
+--установить цену для всех конкретных товаров цену 20
+
