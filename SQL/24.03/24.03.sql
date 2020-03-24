805c25c5961b143468ddeff69dffc494b2951b7a
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-03-24 19:48:10 +0300

    add something

--- SQL/24.03/24.03.sql
+++ SQL/24.03/24.03.sql
@@ -26,3 +26,51 @@ CITY ='London' AND COMM =(
 -------------------------------------------------------------------------------
 
 --найти средний руйтинг покупателей в каждом городе
+SELECT CITY,   --вывести города
+AVG(RATING)    --средний  RATING
+FROM customers
+GROUP BY 
+CITY;
+-------------------------------------------------------------------------------
+--Объеденим все таблицы в одну
+SELECT *
+FROM salespeople, orders, customers
+WHERE salespeople.SNUM = orders.SNUM
+AND orders.CNUM = customers.CNUM;
+
+--расчитать средний рейтинг покупателей для каждого продовца
+SELECT
+    SNAME,
+    AVG(RATING)
+FROM 
+    salespeople, 
+    orders, 
+    customers
+WHERE
+    salespeople.SNUM = orders.SNUM AND orders.CNUM = customers.CNUM 
+GROUP BY 
+    SNAME;
+
+--найти среднюю сумму заказов для каждого продовца на каждую дату
+SELECT
+    ODATE,
+    SNAME,
+COUNT(*) AS 'Количество заказазов',
+AVG(AMT)
+FROM 
+    salespeople, 
+    orders
+WHERE
+    salespeople.SNUM = orders.SNUM  
+GROUP BY
+    SNAME, ODATE;
+
+--средняя сумма заказов на каждое число
+SELECT
+    ODATE,
+    AVG(AMT)
+FROM 
+    orders
+GROUP BY 
+    ODATE;
+    
