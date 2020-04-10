7ec8c2c39c8c0134547585cdea847b92c8409cba
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-04-10 19:06:35 +0300

    create cookie2

---
+++ cookie2/1.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+$_SESSION['time'] = time();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Сессия пошла</h1><br>
+    <a href="2.php">Вторая страница</a>
+
+</body>
+
+</html>
