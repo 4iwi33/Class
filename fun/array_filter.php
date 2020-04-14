2d68ac5cfdb6733d21a0a748e9e445a2f448c5a7
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-04-14 15:47:33 +0300

    updt

--- fun/array_filter.php
+++ fun/array_filter.php
@@ -1,14 +1,4 @@
 <pre>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-
 <?php
 function odd($var)
 {
@@ -29,6 +19,3 @@ echo "Четные   :\n";
 print_r(array_filter($array2, "even"));
 ?>
 </pre>
-</body>
-
-</html>
