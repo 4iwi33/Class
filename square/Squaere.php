2aab7a019a06f3ec1ca5c67039c7a420e3ddae7f
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-04-14 21:25:05 +0300

    add

---
+++ square/Squaere.php
@@ -0,0 +1,19 @@
+<?php
+class Squaere 
+{
+    private $a;
+    public function __construct($a)
+    {
+        $this->a = $a;
+    }
+
+    public function perimetr()
+    {
+        return $this->a * 4;
+    }
+    public function area()
+    {
+        return $this->a ** 2;
+    }
+}
+?>
---
+++ square/result.php
@@ -0,0 +1,9 @@
+<?php
+include("autoload.php");
+
+$result = new Squaere(5);
+
+echo $result->perimetr() . "<br>";
+
+echo $result->area();
+?>
