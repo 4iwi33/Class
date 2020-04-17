1002933fbfc85d7a7c35074e1bfb28183a299823
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-04-17 14:10:07 +0300

    updt

--- calculator2/autoload.php
+++ calculator2/autoload.php
@@ -1,5 +1,7 @@
 <?php
 //автозагрузка
+// "spl_autoload_register" — Регистрирует заданную функцию в качестве реализации метода __autoload()
+
     spl_autoload_register(function($class_name)
     {
         include $class_name . '.php';
