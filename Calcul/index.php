1804f89d019bedf65eee8ccd8bba5964cb7edf4f
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-07-31 20:19:56 +0300

    updt

---
+++ Calcul/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Калькулятор</h1>
+    <form method="" action="">
+        <input type="text" name="">
+        <select name="">
+            <option value="div">/</option>
+            <option value="mul">*</option>
+            <option value="sum">+</option>
+            <option value="dif">-</option>
+        </select>
+        <input type="text" name="">
+        <input type="submit" value="результат">
+    </form>
+</body>
+
+</html>
